eb9a9e5f21427ce907f42274fffde95b0dfa3f7e
Fondzefe Fonyuy Theophile <fondzefe.fonyuy.theophile@sap.com>
2026-03-14 21:44:21 +0100

    Fixes controller initialization

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4022221576"
+    serviceWorkerVersion: "2228105352"
   }
 });
