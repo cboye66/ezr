0bd023481b29dc9a015775524e39087f5f043731
Fondzefe Fonyuy Theophile <fondzefe.fonyuy.theophile@sap.com>
2026-03-16 00:15:18 +0100

    Dashboard updates also

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2228105352"
+    serviceWorkerVersion: "281103217"
   }
 });
